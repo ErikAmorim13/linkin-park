db9f6dd92c3bede5e8812f5899305731cb8908fd
ErikAmorim13 <erik.rodrigues@sptech.school>
2022-11-29 15:16:59 -0300

    Corrigido bug na avaliação

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "linkin",
-    user: "root",
-    password: "@Saopaulo633",
+    user: "aluno",
+    password: "sptech",
 };
 
 function executar(instrucao) {
